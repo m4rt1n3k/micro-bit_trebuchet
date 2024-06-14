27b8979854c0e91407ccffd7fc1d92c0a58e1554
Vladislav Martinek <11995407+m4rt1n3k@users.noreply.github.com>
2024-06-14 18:40:30 +0200

    sound on error button press; fire on clap

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+function A () {
+    if (je_napruženo == 0) {
+        NAPRUŽIT()
+        je_napruženo = 1
+        basic.showIcon(IconNames.Yes)
+    } else {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
+    }
+}
 function NATÁHNOUT () {
     servos.P1.setAngle(0)
     basic.pause(délka_natažení)
@@ -9,23 +18,14 @@ function ODJISTIT () {
     servos.P0.setAngle(90)
 }
 input.onButtonPressed(Button.A, function () {
-    if (je_napruženo == 0) {
-        NAPRUŽIT()
-        je_napruženo = 1
-        basic.showIcon(IconNames.Yes)
-    }
+    A()
 })
 function ZAJISTIT () {
     servos.P0.setAngle(0)
     basic.pause(300)
     servos.P0.setAngle(90)
 }
-function NAPRUŽIT () {
-    servos.P1.setAngle(180)
-    basic.pause(délka_natažení)
-    servos.P1.setAngle(90)
-}
-input.onButtonPressed(Button.B, function () {
+function B () {
     if (je_napruženo == 1) {
         basic.showIcon(IconNames.Skull)
         ODJISTIT()
@@ -33,11 +33,24 @@ input.onButtonPressed(Button.B, function () {
         ZAJISTIT()
         je_napruženo = 0
         basic.showIcon(IconNames.Triangle)
+    } else {
+        music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
     }
+}
+function NAPRUŽIT () {
+    servos.P1.setAngle(180)
+    basic.pause(délka_natažení)
+    servos.P1.setAngle(90)
+}
+input.onSound(DetectedSound.Loud, function () {
+    A()
+    B()
+})
+input.onButtonPressed(Button.B, function () {
+    B()
 })
 let je_napruženo = 0
 let délka_natažení = 0
-basic.showString("Ready")
 délka_natažení = 1800
 je_napruženo = 0
 basic.showIcon(IconNames.Triangle)
